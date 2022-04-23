c0f61f3010fbe71d82e46b56794ef440398f2a80
Carolina <caro09.2003@hotmail.com>
2022-04-22 21:14:48 -0500

    Adding a player (Daisy) and motion

--- JuegoLaberinto/Game1.cs
+++ JuegoLaberinto/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace JuegoLaberinto
 {
@@ -8,17 +10,28 @@ namespace JuegoLaberinto
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        
+        Player Daisy;
+        
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            //_graphics.IsFullScreen = true;
+            _graphics.PreferredBackBufferWidth = 1500;
+            _graphics.PreferredBackBufferHeight = 1000;
+            _graphics.ApplyChanges();
+
         }
 
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+           
+            Daisy = new Player();
+            
 
             base.Initialize();
         }
@@ -28,23 +41,52 @@ namespace JuegoLaberinto
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+           
+            Daisy.LoadContent(this.Content);
+            
+
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+            KeyboardState myKeyboard = Keyboard.GetState();
 
             // TODO: Add your update logic here
+
+            if (myKeyboard.IsKeyDown(Keys.Left))
+            {
+                Daisy.Move("left");
+            }
+            else if (myKeyboard.IsKeyDown(Keys.Right))
+            {
+                Daisy.Move("right");
+            }
+            else if (myKeyboard.IsKeyDown(Keys.Down))
+            {
+                Daisy.Move("down");
+            }
+            else if (myKeyboard.IsKeyDown(Keys.Up))
+            {
+                Daisy.Move("up");
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            
+            Daisy.Draw(this._spriteBatch, Color.White);
+            
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- JuegoLaberinto/Player.cs
+++ JuegoLaberinto/Player.cs
@@ -2,9 +2,44 @@
 using System.Collections.Generic;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
 namespace JuegoLaberinto
 {
-    class Player
+    class Player : Sprite
     {
+        public Player() : base("Daisy", new Point(0, 770), new Point(200, 200))
+        {
+
+        }
+
+        /// <summary>
+        /// Move the object horizontally and vertically
+        /// /// </summary>
+        /// <param name="direction">string, right, left, down and up</param>
+
+        public void Move(string direction)
+        {
+            switch (direction)
+            {
+                case "right":
+                    this.Location = new Point(this.Location.X + 5, this.Location.Y);
+                    break;
+                case "left":
+                    this.Location = new Point(this.Location.X - 5, this.Location.Y);
+                    break;
+                case "down":
+                    this.Location = new Point(this.Location.X, this.Location.Y + 5);
+                    break;
+                case "up":
+                    this.Location = new Point(this.Location.X, this.Location.Y - 5);
+                    break;
+
+            }
+
+        }
     }
 }
